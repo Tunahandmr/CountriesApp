98456bc1124cfe7654dc0256fc987d52a078439c
Tunahan Demir <tunahandemir818@gmail.com>
2022-09-29 02:34:35 +0300

    interface created

---
+++ app/src/main/java/com/tunahan/countriesapp/view/CountryFragment.kt
@@ -0,0 +1,36 @@
+package com.tunahan.countriesapp.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.tunahan.countriesapp.R
+
+
+class CountryFragment : Fragment() {
+
+    private var countryUuid = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_country, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            countryUuid = CountryFragmentArgs.fromBundle(it).countryUuid
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/tunahan/countriesapp/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tunahan.countriesapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tunahan.countriesapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
